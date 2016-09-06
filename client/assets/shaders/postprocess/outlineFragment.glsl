26b8ff7d625a86e054308b8013c0526e6e9b9e0d
arksu <ark@ark.su>
2016-09-06 12:17:03 +0500

    delete old code

--- client/assets/shaders/postprocess/outlineFragment.glsl
+++ client/assets/shaders/postprocess/outlineFragment.glsl
@@ -3,21 +3,11 @@ out vec4 fragColor;
 uniform sampler2D u_texture;
 uniform sampler2D u_textureDepth;
 
-//in vec2 v_texCoords0;
-//in vec2 v_texCoords1;
-//in vec2 v_texCoords2;
-//in vec2 v_texCoords3;
-//in vec2 v_texCoords4;
-
 in vec2 v_texCoords0;
 in vec2 v_texCoords1;
 in vec2 v_texCoords2;
 in vec2 v_texCoords3;
 in vec2 v_texCoords4;
-in vec2 v_texCoords5;
-in vec2 v_texCoords6;
-in vec2 v_texCoords7;
-in vec2 v_texCoords8;
 
 float unpack_depth(const in vec4 rgba_depth){
 	vec4 pd = rgba_depth;
@@ -45,33 +35,7 @@ float unpack_depth2(const in vec4 rgba_depth){
     return depth;
 }
 
-void main(){
-//	float depth = (texture(u_textureDepth, v_texCoords0).a
-//        + texture(u_textureDepth, v_texCoords1).a
-//        + texture(u_textureDepth, v_texCoords2).a
-//    + texture(u_textureDepth, v_texCoords3).a
-//    + texture(u_textureDepth, v_texCoords4).a
-//    + texture(u_textureDepth, v_texCoords5).a
-//    + texture(u_textureDepth, v_texCoords6).a
-//    + texture(u_textureDepth, v_texCoords7).a
-//    + texture(u_textureDepth, v_texCoords8).a) / 9.0;
-
-//    float depth = abs(
-//      unpack_depth(texture(u_textureDepth, v_texCoords0))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords1))
-//    - unpack_depth(8 * texture(u_textureDepth, v_texCoords2))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords3))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords4))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords5))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords6))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords7))
-//    + unpack_depth(texture(u_textureDepth, v_texCoords8))
-//    ) ;
-
-//	float d = unpack_depth(texture(u_textureDepth, v_texCoords2));
-//	float visibility = exp(-pow((d * 0.01), 2.5));
-//	float visibility = d / 3;//-pow(d * 0.01, 2.5);
-
+void main() {
     float depth =
         abs(unpack_depth(texture(u_textureDepth, v_texCoords0))
     + unpack_depth(texture(u_textureDepth, v_texCoords1))
